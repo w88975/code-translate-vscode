2ec15a721ee7111303df331dbd2748fd3bdf94f2
KamiSama <1457434343@qq.com>
2020-09-05 03:10:04 +0800

    fix eslint

--- extension.js
+++ extension.js
@@ -1,14 +1,8 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
 const translate = require('./src/index.js');
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
 /**
  * @param {vscode.ExtensionContext} context
  */
-async function activate(context) {
+async function activate() {
     translate.init()
 }
 exports.activate = activate;
