f26dad8cba2f6f86beff89897c056e31a1e1377e
KamiSama <1457434343@qq.com>
2020-09-10 11:58:45 +0800

    字符串拆分单词 组合词拆分优化

--- src/format.js
+++ src/format.js
@@ -4,14 +4,21 @@ module.exports = {
     // 单词拆分
     // 例: foor-bar 拆分为 [foo, bar]
     getWordArray: function (character) {
-        if (!character) {
+        let formatChar = character
+        var capitalizes = formatChar.match(/[A-Z\s]{2,}/g)
+        if (capitalizes && capitalizes.length) {
+            capitalizes.map(item => {
+                formatChar = formatChar.replace(item, humps.pascalize(item.toLowerCase()))
+            })
+        }
+        if (!formatChar) {
             return
         }
         // 判断是否全大写
         if (/^[A-Z]+$/.test(character)) {
             return [character.toLowerCase()]
         }
-        return (humps.decamelize(humps.camelize(character), { separator: '|' })).split('|')
+        return Array.from(new Set(humps.decamelize(humps.camelize(formatChar), { separator: '|' }).split('|')))
     },
     cleanWord: function (character) {
         return character.replace(/"/g, '')
