cdfc01b03f38642b51da1c3570a75ca11c06af0f
KamiSama <1457434343@qq.com>
2021-10-28 01:37:58 +0800

    remove humps module; web extension support!

--- src/format.js
+++ src/format.js
@@ -1,26 +1,26 @@
 
-const humps = require('humps')
+const humps = require('./humps')
 module.exports = {
-    // 单词拆分
-    // 例: foor-bar 拆分为 [foo, bar]
-    getWordArray: function (character) {
-        let formatChar = character
-        var capitalizes = formatChar.match(/[A-Z\s]{2,}/g)
-        if (capitalizes && capitalizes.length) {
-            capitalizes.map(item => {
-                formatChar = formatChar.replace(item, humps.pascalize(item.toLowerCase()))
-            })
-        }
-        if (!formatChar) {
-            return
-        }
-        // 判断是否全大写
-        if (/^[A-Z]+$/.test(character)) {
-            return [character.toLowerCase()]
-        }
-        return Array.from(new Set(humps.decamelize(humps.camelize(formatChar), { separator: '|' }).split('|')))
-    },
-    cleanWord: function (character) {
-        return character.replace(/"/g, '')
+  // 单词拆分
+  // 例: foor-bar 拆分为 [foo, bar]
+  getWordArray: function (character) {
+    let formatChar = character
+    var capitalizes = formatChar.match(/[A-Z\s]{2,}/g)
+    if (capitalizes && capitalizes.length) {
+      capitalizes.map(item => {
+        formatChar = formatChar.replace(item, humps.pascalize(item.toLowerCase()))
+      })
     }
+    if (!formatChar) {
+      return
+    }
+    // 判断是否全大写
+    if (/^[A-Z]+$/.test(character)) {
+      return [character.toLowerCase()]
+    }
+    return Array.from(new Set(humps.decamelize(humps.camelize(formatChar), { separator: '|' }).split('|')))
+  },
+  cleanWord: function (character) {
+    return character.replace(/"/g, '')
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,56 +1,56 @@
-const vscode = require('vscode');
+const vscode = require('vscode')
 const DICTQuery = require('./query')
 const formatter = require('./format')
 
 const markdownHeader = `翻译 \`$word\` :  
-`;
+`
 const markdownFooter = `  
-`;
+`
 const markdownLine = `  
 *****
-`;
+`
 
 const genMarkdown = function (word, translation) {
-    if (!translation) {
-        return `- [${word}](https://translate.google.cn?text=${word}) :  
-本地词库暂无结果 , 查看 [Google翻译](https://translate.google.cn?text=${word}) [百度翻译](https://fanyi.baidu.com/#en/zh/${word})`
-    }
+  if (!translation) {
     return `- [${word}](https://translate.google.cn?text=${word}) :  
+本地词库暂无结果 , 查看 [Google翻译](https://translate.google.cn?text=${word}) [百度翻译](https://fanyi.baidu.com/#en/zh/${word})`
+  }
+  return `- [${word}](https://translate.google.cn?text=${word}) :  
 ${translation.replace(/\\n/g, `  
-`)}`;
+`)}`
 }
 
 async function init() {
-    vscode.languages.registerHoverProvider('*', {
-        async provideHover(document, position) {
-            if (!document.getWordRangeAtPosition(position)) {
-                return
-            }
-            let word = document.getText(document.getWordRangeAtPosition(position));
-            let selectText = vscode.window.activeTextEditor.document.getText(vscode.window.activeTextEditor.selection);
-            if (selectText && word.indexOf(selectText) > -1) {
-                word = selectText
-            }
-            let originText = formatter.cleanWord(word)
-            let words = formatter.getWordArray(formatter.cleanWord(word))
-            let hoverText = ''
-            for (let i = 0; i < words.length; i++) {
-                let _w = words[i]
-                let ret = await DICTQuery(_w)
-                if (i == 0) {
-                    hoverText += genMarkdown(_w, ret)
-                } else {
-                    hoverText += markdownLine + genMarkdown(_w, ret)
-                }
-            }
-            const header = markdownHeader.replace('$word', originText)
-            hoverText = header + hoverText + markdownFooter
-            return new vscode.Hover(hoverText)
+  vscode.languages.registerHoverProvider('*', {
+    async provideHover(document, position) {
+      if (!document.getWordRangeAtPosition(position)) {
+        return
+      }
+      let word = document.getText(document.getWordRangeAtPosition(position))
+      let selectText = vscode.window.activeTextEditor.document.getText(vscode.window.activeTextEditor.selection)
+      if (selectText && word.indexOf(selectText) > -1) {
+        word = selectText
+      }
+      let originText = formatter.cleanWord(word)
+      let words = formatter.getWordArray(formatter.cleanWord(word))
+      let hoverText = ''
+      for (let i = 0; i < words.length; i++) {
+        let _w = words[i]
+        let ret = await DICTQuery(_w)
+        if (i == 0) {
+          hoverText += genMarkdown(_w, ret)
+        } else {
+          hoverText += markdownLine + genMarkdown(_w, ret)
         }
-    })
+      }
+      const header = markdownHeader.replace('$word', originText)
+      hoverText = header + hoverText + markdownFooter
+      return new vscode.Hover(hoverText)
+    }
+  })
 }
 
 
 module.exports = {
-    init
+  init
 }
